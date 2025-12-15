4d19dba3c2768561f25e63fe927c14b721666d4c
Kamil <kmirosz6@gmail.com>
2025-12-15 09:21:08 +0100

    Refactor bite and hide methods for improved readability by adjusting formatting

--- app/animals/carnivore.py
+++ app/animals/carnivore.py
@@ -5,6 +5,8 @@ from app.animals.herbivore import Herbivore
 class Carnivore(Animal):
 
     @staticmethod
-    def bite(animal: Animal) -> None:
+    def bite(
+        animal: Animal
+    ) -> None:
         if isinstance(animal, Herbivore) and not animal.hidden:
             animal.health -= 50
--- app/animals/herbivore.py
+++ app/animals/herbivore.py
@@ -3,5 +3,7 @@ from app.animals.base.animal import Animal
 
 class Herbivore(Animal):
 
-    def hide(self) -> None:
+    def hide(
+        self
+    ) -> None:
         self.hidden = not self.hidden
