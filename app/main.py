7a52f8c33a344a2966c35a3cec91235a4e02fd26
Kamil <kmirosz6@gmail.com>
2025-12-15 09:14:59 +0100

    Refactor animal interactions in main.py to use assertions for health checks and update bite method to accept Animal type

--- app/main.py
+++ app/main.py
@@ -6,18 +6,18 @@ from app.animals.base.animal import Animal
 if __name__ == "__main__":
     lion = Carnivore("Lion King")
     rabbit = Herbivore("Susan")
-    rabbit.health == 100
+    assert rabbit.health == 100
     lion.bite(rabbit)
-    rabbit.health == 50
+    assert rabbit.health == 50
 
     rabbit.hide()
     lion.bite(rabbit)
-    rabbit.health == 50
+    assert rabbit.health == 50
 
     print(rabbit)
 
     rabbit.hide()
     lion.bite(rabbit)
-    rabbit.health == 0
+    assert rabbit.health == 0
 
-    rabbit in Animal.alive
+    assert rabbit not in Animal.alive
